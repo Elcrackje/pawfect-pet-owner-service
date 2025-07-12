9e2f90610efe6025b4180df92c60fa0a6012a3cd
Jose Gutierrez <josecitogutigar21@gmail.com>
2025-07-12 03:38:47 -0500

    feat: add petowner service

---
+++ src/test/java/pe/upc/pawfectcaremicroservices/petownerservice/PetPetOwnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.pawfectcaremicroservices.petownerservice;
+
+import org.junit.jupiter.api.Test;
+
+class PetPetOwnerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test básico que siempre pasa
+		System.out.println("Test ejecutado correctamente.");
+	}
+
+}
